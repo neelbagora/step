aa9160dd93ddd5f8bb155d88807f9a72c5a197d3
Neel Bagora <neelbagora@gmail.com>
2020-05-27 11:07:35 -0700

    Style webpage and add images related to aviation.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,18 +11,65 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+filterSelection("all")
+function filterSelection(c) {
+    updateGalleryText(c);
+    var x, i;
+    x = document.getElementsByClassName("column");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    }
+}
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    }
+}
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+function removeClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);     
+        }
+    }
+    element.className = arr1.join(" ");
+}
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+function updateGalleryText(elementName) {
+    const airplane = "These are photos I have taken from the flight simulator, X-Plane 11. The callsigns mentioned below are callsigns used on the VATSIM online air traffic network.";
+    const projects = "These are pictures of the projects I have worked on throughout the years. I am mainly experienced in Java, C, C++, and Python.";
+    const miscellaneous = "These are pictures that I thought were cool but could not categorize.";
+    const showAll = "These are all the pictures that I have posted on this website, pictures can be filtered using the menu bar above.";
+    if (elementName === 'all'){
+        document.getElementById('gallery-text').innerText = showAll;
+    }
+    else if (elementName === 'airplanes') {
+        document.getElementById('gallery-text').innerText = airplane;
+    }
+    else if (elementName === 'projects') {
+        document.getElementById('gallery-text').innerText = projects;
+    }
+    else {
+        document.getElementById('gallery-text').innerText = miscellaneous;
+    }
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = document.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
